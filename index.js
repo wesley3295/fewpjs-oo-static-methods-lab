dceb2030bca7a85a81558d53b02eb9032e758bf7
Wesley <wspencer3295@yahoo.com>
2021-03-01 22:15:30 -0700

    commit tree 1b0dc2ecb442933fd2339a4e837c4529107bc177

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+  
+  static titleize(string){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arrayOfWords = string.split(" ")
+    let titleizedString = []
+   for(let i = 0 ; i<arrayOfWords.length;i++){
+     if(i==0){
+       titleizedString.push(Formatter.capitalize(arrayOfWords[i]))
+     }else{
+       if (exceptions.includes(arrayOfWords[i])){
+         titleizedString.push(arrayOfWords[i])
+       }else{
+       titleizedString.push(Formatter.capitalize(arrayOfWords[i]))
+
+       }
+     }
+   }
+  return titleizedString.join(" ")
+  }
+  
 }
